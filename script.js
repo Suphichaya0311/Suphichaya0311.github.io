31137f76863a10fed321229569e0acacf4e03417
Suphichaya0311 <oil.0877611417@gmail.com>
2025-12-17 19:01:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// หัวใจคลิกแล้วเด้ง
+document.addEventListener("click",e=>{
+  if(e.target.classList.contains("heart")){
+    e.target.classList.toggle("active");
+  }
+});
 
+// ปฏิทินคลิกแล้วรูปเด้ง
+function showPic(img){
+  document.getElementById("popupImg").src = img;
+  document.getElementById("popup").style.display="flex";
+}
+function closePic(){
+  document.getElementById("popup").style.display="none";
+}
